e508da8603f1266bce7f69d6eefc19fec9ca04fa
Jakob Persons <jakobep24@gmail.com>
2021-01-06 19:22:57 -0500

    more db changes

---
+++ db/migrate/20210106232528_remove_column_deadline_in_tasks.rb
@@ -0,0 +1,5 @@
+class RemoveColumnDeadlineInTasks < ActiveRecord::Migration[6.1]
+  def change
+    change_column :tasks, :deadline, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_213052) do
+ActiveRecord::Schema.define(version: 2021_01_06_232528) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_01_06_213052) do
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deadline"
+    t.date "deadline"
     t.integer "skill_id"
   end
 
