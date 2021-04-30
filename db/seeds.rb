7bd2e7e1613852d37daa4ace8e248077f06c19d2
Jakob Persons <jakobep24@gmail.com>
2021-04-30 15:15:45 -0400

    reseeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# puts "Destroy everything!"
-# User.destroy_all
-# Worker.destroy_all
-# Skill.destroy_all
-# WorkerSkill.destroy_all
+puts "Destroy everything!"
+User.destroy_all
+Worker.destroy_all
+Skill.destroy_all
+WorkerSkill.destroy_all
 
-# puts "Creating Users!"
-# 5.times do
-#    User.create(name: Faker::FunnyName.name, username: Faker::Games::Zelda.character, password: "abc123")
-# end
+puts "Creating Users!"
+5.times do
+   User.create(name: Faker::FunnyName.name, username: Faker::Games::Zelda.character, password: "abc123")
+end
 
-# puts "Creating Workers!"
-# 15.times do 
-#    Worker.create(name: Faker::FunnyName.name)
-# end
+puts "Creating Workers!"
+15.times do 
+   Worker.create(name: Faker::FunnyName.name)
+end
 
 puts "Learning skills!"
-# Skill.create(name: "Painter")
-# Skill.create(name: "Graphic Design")
-# Skill.create(name: "Carpenter")
-# Skill.create(name: "Chef")
-# Skill.create(name: "Analytics")
+Skill.create(name: "Painter")
+Skill.create(name: "Graphic Design")
+Skill.create(name: "Carpenter")
+Skill.create(name: "Chef")
+Skill.create(name: "Analytics")
 Skill.create(name: "Digital Marketing")
 Skill.create(name: "Programming")
 Skill.create(name: "Translation")
@@ -36,7 +36,7 @@ Skill.create(name: "Illustration")
 Skill.create(name: "Video Explainer")
 
 
-# puts "Workers learning skills!"
-# 5.times do 
-#    WorkerSkill.create(skill_id: Skill.all.sample.id, worker_id: Worker.all.sample.id)
-# end
+puts "Workers learning skills!"
+5.times do 
+   WorkerSkill.create(skill_id: Skill.all.sample.id, worker_id: Worker.all.sample.id)
+end
